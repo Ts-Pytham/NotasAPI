7113b0e277a9625d31b5dd8087312940af867aa8
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-09-29 23:16:05 -0500

    Creación del Controlador de Recordatorios

---
+++ Controllers/RecordatorioController.cs
@@ -0,0 +1,40 @@
+﻿namespace NotasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecordatorioController : ControllerBase
+    {
+        private readonly IRecordatorioBusiness _recordatorioBusiness;
+
+        public RecordatorioController(IRecordatorioBusiness recordatorioBusiness)
+        {
+            _recordatorioBusiness = recordatorioBusiness;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Response<IEnumerable<RecordatorioDTO>>>> GetRepositorios(int idUsuario)
+        {
+            var result = await _recordatorioBusiness.GetAllRecordatorios(idUsuario);
+
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return NotFound(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RecordatorioInsertDTO>> CreateRecordatorio(int idUsuario, RecordatorioInsertDTO insertDTO)
+        {
+            var result = await _recordatorioBusiness.CreateRecordatorio(idUsuario, insertDTO);
+            
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return NotFound(result);
+        }
+    }
+}
