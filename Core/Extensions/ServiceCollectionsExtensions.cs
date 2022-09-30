79e85ea074b3a8c81223e494ef8050bf87bae022
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-09-29 23:17:53 -0500

    Inyección de dependencias de Recordatorio

--- Core/Extensions/ServiceCollectionsExtensions.cs
+++ Core/Extensions/ServiceCollectionsExtensions.cs
@@ -5,13 +5,14 @@ public static class ServiceCollectionsExtensions
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddTransient<IUsuarioRepository, UsuarioRepository>()
-                ;
+                .AddTransient<IRecordatorioBusiness, RecordatorioBusiness>();
         return services;
     }
 
     public static IServiceCollection AddBusiness(this IServiceCollection services)
     {
-        services.AddTransient<IUsuarioBusiness, UsuarioBusiness>();
+        services.AddTransient<IUsuarioBusiness, UsuarioBusiness>()
+                .AddTransient<IRecordatorioRepository, RecordatorioRepository>();
         return services;
     }
 }
